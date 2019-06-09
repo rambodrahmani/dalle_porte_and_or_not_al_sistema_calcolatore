acf27fc18e557faa1e8b8ece9b9249d0ad2541ec
Rambod Rahmani <rambodrahmani@autistici.org>
2019-06-09 10:04:42 +0200

    Add D flip flop Verilog implementation using reg variable and ModelSim simulation screenshots.

---
+++ verilog/capitolo_iii/D_flip_flop_Reg.v
@@ -0,0 +1,30 @@
+/**
+ *
+ * File: D_flip_flop_Reg.v
+ *       
+ *       
+ * Author: Rambod Rahmani <rambodrahmani@autistici.org>
+ *         Created on 09/06/2019.
+ *
+ */
+
+`include "Clock.v"
+
+module D_flip_flop_Reg(d, q, reset_);
+    input reset_;
+    input d;
+    output q;
+
+    wire c;
+    Clock clock(c);
+
+    reg D_EDGE;
+
+    assign q = D_EDGE;
+
+    always @(reset_ == 0) #1 D_EDGE <= 0;
+
+    always @(posedge c) if (reset_ == 1) #3 D_EDGE <= (c == 1) ? d:D_EDGE;
+    
+endmodule
+
